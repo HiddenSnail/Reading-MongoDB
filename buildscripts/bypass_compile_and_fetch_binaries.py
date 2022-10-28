6cf0199949d9d3e5e1864decdcb88c5cdbfbb12f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-28 19:43:47 +0000

    Adding tarfile member sanitization to extractall()

--- buildscripts/bypass_compile_and_fetch_binaries.py
+++ buildscripts/bypass_compile_and_fetch_binaries.py
@@ -369,7 +369,26 @@ def fetch_artifacts(build: Build, revision: str):
                 ]
                 LOGGER.info("Extracting the files...", filename=filename,
                             files="\n".join(tarinfo.name for tarinfo in subdir))
-                tar.extractall(members=subdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, members=subdir)
         elif filename.startswith("mongo-src"):
             LOGGER.info("Retrieving mongo source", filename=filename)
             # This is the distsrc.[tgz|zip] as referenced in evergreen.yml.
